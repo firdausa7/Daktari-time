8e1d72ded8727ea286f6689a42e5f563fa85487a
firdausa7 <firdausa.salat@gmail.com>
2019-07-30 15:44:01 +0300

    add slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,10 @@ function showSlides() {
     slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
+  if (slideIndex > slides.length) {slideIndex = 1
+        
   }
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 2000); // Change image every 2 seconds}
 }
 });
