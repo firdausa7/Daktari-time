26343274beb1e16842ff0db5713194a799fc03af
WendyOmollo <omollow280@gmail.com>
2019-08-01 20:27:35 +0300

    Make the specialists div functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){  
-  $("form#details").submit(function(event){
-    event.preventDefault();
-    var name = $("input#letters").val();
-      $("p.show-details").text("Thank you " + name + " your message has been received.We'll get back to you shortly.");
-  });   
+  $("li#onco").click(function(){
+    $("#oncologists").show();
+    $("#paediatricians").hide();
+  });
+  $("li#gynae").click(function(){
+  $("#gynaecologists").show();
+  $("#oncologists").hide();
+  $("#paediatricians").hide();
+  });
 });   
