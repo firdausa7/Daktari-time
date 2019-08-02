baff2a6472b20c23f12f6516f0f86e05b98db560
firdausa7 <firdausa.salat@gmail.com>
2019-08-02 13:22:43 +0300

    add new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 
-$('document').ready(function(){
-function myForm() {  
-   var Name = document.forms["commentForm"]["entername"].value;
-  if (Name== "") {
-    alert("Name must be filled out");
-    return false
-  };
-  var Email = document.forms["commentForm"]["enteremail"].value;
-  if(Email== ""){
-      alert("Email must be filled out");
-      return false
-  };
-  var Com = document.forms['commentForm']['entercomment'].value;
-  if(Com == ""){
-      alert("Please type in your comments");
-      return false
-  }
-  else {
-    alert("Hi "+ Name +' Thank you for writing to us.We value your feedback.')
-  };
+$(document).ready(function(){
+// function myForm() {  
+//    var Name = document.forms["commentForm"]["entername"].value;
+//   if (Name== "") {
+//     alert("Name must be filled out");
+//     return false
+//   };
+//   var Email = document.forms["commentForm"]["enteremail"].value;
+//   if(Email== ""){
+//       alert("Email must be filled out");
+//       return false
+//   };
+//   var Com = document.forms['commentForm']['entercomment'].value;
+//   if(Com == ""){
+//       alert("Please type in your comments");
+//       return false
+//   }
+//   else {
+//     alert("Hi "+ Name +' Thank you for writing to us.We value your feedback.')
+//   };
   
       $('#day1').click(function(){
         $('.time').toggle();
@@ -62,9 +62,9 @@ function myForm() {
 
 
 //booking buttons
-$('#book').click(function(){
-    $()
-})
+// $('#book').click(function(){
+//     $()
+// })
 $('button#book1').click(function(){
     $('button#book1').hide();
     $('button#book2').hide();
@@ -110,22 +110,22 @@ $('button#book5').click(function(){
     $('#choose').hide();
     $('#p').text("Your Appointment has been booked 04:00pm");
 });
-var slideIndex = 0;
-showSlides();
+// var slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1
         
-  }
-  slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 50000); // Change image every 2 seconds}
-}
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//     setTimeout(showSlides, 50000); // Change image every 2 seconds}
+// }
 
   $("#worker1").click(function(){
     $("#comment1").toggle();
